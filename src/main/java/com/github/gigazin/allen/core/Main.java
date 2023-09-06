953e50d43a07afa71b7f7efc943d1f043e5c4e00
gigazin <gigazin@hotmail.com>
2023-09-06 19:20:56 -0300

    first build to test launcher outside IDE

---
+++ src/main/java/com/github/gigazin/allen/core/Main.java
@@ -0,0 +1,9 @@
+package com.github.gigazin.allen.core;
+
+import com.github.gigazin.allen.launcher.AllenLauncher;
+
+public class Main {
+    public static void main(String[] args) {
+        AllenLauncher.main(args);
+    }
+}
